687f4b9273909d7b37b5648f9fd26007c07072ea
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-09-07 12:35:13 -0500

    select squares

--- Assets/Script/EnglishGame.cs
+++ Assets/Script/EnglishGame.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnglishGame : MonoBehaviour
 {
     public GameObject mainPanel;
-
     public GameObject sudokuFieldPanel;
-
     public GameObject fieldPrefab;
 
-     void Start()
+    // Dictionary to store the field prefabs using Vector2Int for grid positions
+    private Dictionary<Vector2Int, FieldPrefabObject> _englishFieldPrefabDictionary =
+        new Dictionary<Vector2Int, FieldPrefabObject>();
+
+    private FieldPrefabObject _currentHoveredFieldPrefab;
+
+    void Start()
     {
-        for (int i = 0; i < 81; i++) 
+        CreateFieldPrefabs();
+    }
+
+    private void CreateFieldPrefabs()
+    {
+        for (int row = 0; row < 9; row++)
         {
-            GameObject.Instantiate(fieldPrefab, sudokuFieldPanel.transform);
+            for (int column = 0; column < 9; column++)
+            {
+                // Instantiate the field prefab and parent it to the sudokuFieldPanel
+                GameObject instance = GameObject.Instantiate(fieldPrefab, sudokuFieldPanel.transform);
+
+                // Create and store the FieldPrefabObject with a unique grid position (row, column)
+                FieldPrefabObject englishFieldPrefabObject = new FieldPrefabObject(instance, row, column);
+                _englishFieldPrefabDictionary.Add(new Vector2Int(row, column), englishFieldPrefabObject);
+
+                // Capture the current instance in the closure to avoid capturing the last instance in the loop
+                instance.GetComponent<Button>().onClick.AddListener(() => OnClick_FieldPrefab(englishFieldPrefabObject));
+            }
         }
+    }
+
+    private void OnClick_FieldPrefab(FieldPrefabObject englishFieldPrefabObject)
+    {
+        Debug.Log($"Clicked on prefab row:{englishFieldPrefabObject.Row}, Column: {englishFieldPrefabObject.Column}");
+
+        // Unset hover mode on the previously hovered field
+        if (_currentHoveredFieldPrefab != null)
+        {
+            _currentHoveredFieldPrefab.UnSetHoverMode();
+        }
+
+        // Set hover mode on the currently clicked field
+        _currentHoveredFieldPrefab = englishFieldPrefabObject;
+        englishFieldPrefabObject.SetHoverMode();
     }
 }
---
+++ Assets/Script/FieldPrefabObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FieldPrefabObject {
+    private int _row;
+    private int _column;
+    private GameObject _instance;
+
+    public FieldPrefabObject(GameObject instance, int row, int column)
+    {
+        _instance = instance;
+        _row = row;
+        _column = column;
+    }
+
+    public int Row { get => _row; set => _row = value; }
+    public int Column { get => _column; set => _column = value; }
+
+    public void SetHoverMode()
+    {
+        _instance.GetComponent<Image>().color = new Color(0.7f,0.99f,0.99f);
+    }
+
+    public void UnSetHoverMode()
+    {
+        _instance.GetComponent<Image>().color = new Color(1f, 1f, 1f);
+    }
+}
